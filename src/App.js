88d61f34998debd24fbf03d1e102d73e195ea132
Karina Finegan <kairosity@users.noreply.github.com>
2021-11-17 17:44:58 +0000

    feat: add initial routes: home, login, signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+
+// components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
