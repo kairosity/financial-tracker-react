ff372f54b77c12a54452414eed9804dae103c93d
Karina Finegan <kairosity@users.noreply.github.com>
2021-11-17 18:50:04 +0000

    feat: add signup form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar'
 
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,55 @@
+import { useState } from 'react'
+
 import styles from './Signup.module.css'
 
 export default function Signup() {
+
+    const [displayName, setDisplayName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(displayName, email, password)
+        setDisplayName('')
+        setEmail('')
+        setPassword('')
+    }
+    
     return (
-        <div>
-            Signup
-        </div>
+        <form
+            className={styles['signup-form']}
+            onSubmit={handleSubmit}
+        >
+            <h2>Sign Up</h2>
+            <label>
+                <span>display name:</span>
+                <input 
+                    type="text"
+                    onChange={(e) => {setDisplayName(e.target.value)}}
+                    value={displayName}
+                />
+            </label>
+            <label>
+                <span>email:</span>
+                <input 
+                    type="text"
+                    onChange={(e) => {setEmail(e.target.value)}}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>password:</span>
+                <input 
+                    type="text"
+                    onChange={(e) => {setPassword(e.target.value)}}
+                    value={password}
+                />
+            </label>
+            <button 
+                className='btn'
+                type='submit'
+            >Sign Up!</button>
+        </form>
     )
 }
