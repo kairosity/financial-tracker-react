f28aca1cf621ff3dc2056704888ebc8873c88165
Karina Finegan <kairosity@users.noreply.github.com>
2021-11-20 15:24:32 +0000

    feat: deployed to firebase b/e

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
     return (
         <nav className={styles.navbar}>
             <ul>
-                <li className={styles.title}>My Money</li>
+                <li className={styles.title}>My Money Tracker</li>
                 {!user && (
                     <>
                         <li><Link to='/login'>Login</Link></li>
